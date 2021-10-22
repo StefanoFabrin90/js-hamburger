0ade39e84c4e49d4810407f3b3f94ac4c18f0e6f
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-22 17:28:01 +0200

    add vaff.............

--- teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -4,9 +4,9 @@ const hamburger = document.querySelector('.header-right > a');
 const navMenu = document.querySelector('.hamburger-menu');
 
 
-hamburger.addEventListener('click', function(){
-    navMenu.classList.toggle('hamburger-menu.active');
-})
+hamburger.addEventListener('click', function() {
+    navMenu.className = ' hamburger-menu.active';
+});
 
 
 
