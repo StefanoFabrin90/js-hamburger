52eb2a290d1b0b36ec539281c4ef2f6a44ec359c
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-22 16:57:42 +0200

    boh non so neanche da dove cominciare

---
+++ teachersboolean-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,15 @@
+// ref
+
+const hamburger = document.querySelector('.header-right > a');
+const navMenu = document.querySelector('hamburger-menu.active');
+
+
+hamburger.addEventListener('click', function(){
+    
+})
+
+
+
+
+
+
